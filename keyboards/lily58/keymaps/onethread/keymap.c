44904f7173c03d4f131b04ce2600fc35af8473ee
Ron Hsu <ron.hsu@reaktor.com>
2021-02-21 18:16:54 -0500

    Add layer taps

--- keyboards/lily58/keymaps/onethread/keymap.c
+++ keyboards/lily58/keymaps/onethread/keymap.c
@@ -9,7 +9,7 @@
 #endif
 
 // Left-hand home row mods
-#define HOME_A LCTL_T(KC_A)
+#define LCTL_TA LCTL_T(KC_A)
 #define LALT_TS LALT_T(KC_S)
 #define LGUI_TD LGUI_T(KC_D)
 #define LSFT_TF LSFT_T(KC_F)
@@ -41,7 +41,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |------+------+------+------+------+------|   [   |    |    ]  |------+------+------+------+------+------|
      * |LShift|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |RShift|
      * `-----------------------------------------/       /     \      \-----------------------------------------'
-     *                   | LAlt | LGUI |Space | /       /       \      \  |Space |BackSP| RGUI |
+     *                   | LAlt | LGUI |Space | /  Tab  /       \Enter \  |Space |BackSP| RGUI |
      *                   |      |      |LOWER |/       /         \      \ |RAISE |      |      |
      *                   `----------------------------'           '------''--------------------'
      */
@@ -51,7 +51,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS, \
   KC_LCTRL, KC_A,   LALT_TS,    LGUI_TD,    LSFT_TF,    KC_G,                     KC_H,    RSFT_TJ,    RGUI_TK,    LALT_TL, RCTL_TSCLN, KC_QUOT, \
   KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_LBRC,  KC_RBRC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT, \
-                        KC_LALT, KC_LGUI, LT(_LOWER, KC_SPC), XXXXXXX,  XXXXXXX, LT(_RAISE, KC_SPC), KC_BSPC, KC_RGUI \
+                        KC_LALT, KC_LGUI, LT(_LOWER, KC_BSPC), KC_TAB,  KC_ENT, LT(_RAISE, KC_SPC), KC_BSPC, KC_RGUI \
 ),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -72,7 +72,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                     KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, \
   KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                   KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_MINS, \
   _______, _______, _______, _______, _______, _______, _______, _______, XXXXXXX, KC_MINS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, \
-                             _______, _______, _______, _______, _______, KC_ENT,  _______, _______\
+                             _______, _______, _______, _______, _______, _______,  _______, _______\
 ),
     /* RAISE
      * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -94,7 +94,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______, \
   _______, _______, _______, _______, _______, _______,                     _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX, \
   _______, _______, _______, _______, _______, _______,  _______, _______,  _______, KC_UNDS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS, \
-                             _______, _______, KC_BSPC,  _______, _______,  _______, _______, _______ \
+                             _______, _______, _______,  _______, _______,  _______, _______, _______ \
 ),
 /* ADJUST
  * ,-----------------------------------------.                    ,-----------------------------------------.
